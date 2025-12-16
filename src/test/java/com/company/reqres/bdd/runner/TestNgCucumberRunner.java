899dee3f0b21bc1590d62d202192fd07cd9c92df
Quispe-Solorzano Joel-Alfonso <joel.quispe@inetum.com>
2025-12-16 18:06:42 -0500

    testRunner-tags-limpio

--- src/test/java/com/company/reqres/bdd/runner/TestNgCucumberRunner.java
+++ src/test/java/com/company/reqres/bdd/runner/TestNgCucumberRunner.java
@@ -18,7 +18,7 @@ import io.cucumber.testng.CucumberOptions;
         "html:target/cucumber-report.html",
         "json:target/cucumber-report.json"},
         monochrome = true,
-        tags = "@Api-Usuarios"
+        tags = ""
         )
 public class TestNgCucumberRunner extends AbstractTestNGCucumberTests {
 
